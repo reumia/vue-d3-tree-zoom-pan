b366a4d17b457f5f664b198e80acc8848e2f6187
ZZOMAN <reumia@gmail.com>
2017-10-27 10:24:56 +0900

    Tree 작성

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Tree/>
+  </div>
+</template>
+
+<script>
+import Tree from './components/Tree'
+
+export default {
+  name: 'app',
+  components: { Tree }
+}
+</script>
